a975b941f4e9ced1b7a3ff12a7b0fc2969c606df
Luke Slater <tinmachin3@gmail.com>
2020-10-26 21:13:54 +0000

    load ann/diag files, show main list

--- index.js
+++ index.js
@@ -5,15 +5,16 @@ var url = require('url'),
     viewTemplate = require('./view.pug'),
     dataTable = require('datatables'),
     FileSaver = require('file-saver'),
+    _ = require('lodash')._,
     results = {},
     userInfo = {},
+    annotations = {},
+    diagnoses = {},
     table;
 
 $(document).ready(function() {
   loadIndex();
 });
-
-// TODO: add progress to main table, fill up results as you go, semi-autosave feature, etc, then done
 
 /*** Functions ***/
 
@@ -24,37 +25,88 @@ var showInfoPopup = function() {
   $("#infoModal").modal();
 };
 
-var saveUserInfo = function() {
+const saveUserInfo = () => {
   userInfo = {
     'name': $("#nameInput").val(),
     'gmc': $("#gmcInput").val(),
     'specialty': $("#specialtyInput").val()
   };
   
-  $("#infoModal").modal('hide');
+  $("#infoModal").modal('hide')
 
-  //var rSplit = userInfo.range.split('-');
-  //trimTable(rSplit[0], rSplit[0]);
+  loadList();
 };
+
+let readFileFactory = (cb) => {
+  return (event) => {
+    const ourFile = event.target.files[0];
+
+    let reader = new FileReader();
+    reader.readAsText(ourFile);
+
+    reader.onload = function() {
+      cb(reader.result);
+    };
+
+    // TODO: make pop up
+    reader.onerror = function() {
+      console.log(reader.error);
+    };
+  }
+}
 
 /*
  * Load Index
  */
-var loadIndex = function(cb) {
-  document.getElementById("content").innerHTML = indexTemplate({});
+const loadIndex = (cb) => {
+  $("#content").html(indexTemplate({}));
   showInfoPopup();
-}
 
-var loadList = function(cb) {
+  // TODO should remove the file reading boilerplate here really
+  $('#annFileInput').bind('change', readFileFactory((text) => {
+    const lines = text.split('\n');
+    _.each(lines, (l) => {
+      const fields = l.split('\t');
+      const uid = fields[0].split('.')[0];
+      if(!_.has(annotations, uid)) {
+        annotations[uid] = [];
+      }
+      annotations[uid].push({
+        iri: fields[1],
+        label: fields[2],
+        matchedText: fields[3],
+        group: fields[4],
+        tags: fields[5],
+        sid: fields[6],
+        sentence: fields[7]
+      });
+    });
+  }));
+  $('#diaFileInput').bind('change', readFileFactory((text) => {
+    const lines = text.split('\n');
+    _.each(lines, (l) => {
+      const fields = l.split('\t');
+      const uid = fields[0].split('.')[0];
+      if(!_.has(diagnoses, uid)) {
+        diagnoses[uid] = [];
+      }
+      diagnoses[uid].push({
+        iri: fields[1],
+        label: fields[2],
+        target: fields[3],
+        status: fields[4]
+      });
+    });
+  }));
+};
+
+const loadList = (cb) => {
   var u = url.parse(window.location.href, true).query;
-  document.getElementById("content").innerHTML = listTemplate({
-    patients: patients
-  });
 
-  document.getElementById("save").onclick = function() {
+  $('#content').html(listTemplate({ diagnoses }));
+  $('#save').onclick = function() { // TODO we need to change this to the tsv with extras
     saveText(JSON.stringify({ 'results': results }), "results.json");
   };
-
 
   $('#plist tfoot th').each(function() {
     var title = $(this).text();
@@ -63,9 +115,6 @@ var loadList = function(cb) {
 
   // Create DataTable
   table = $('#plist').DataTable({ 'iDisplayLength': 100 });
-
-  // TODO: mark done patients
-  // I thought that was already done?
 
   // Apply the search
   table.columns().every(function() {
@@ -81,21 +130,8 @@ var loadList = function(cb) {
   });
 
   resetScroll();
-
-  };
-  
-/*var trimTable = function(start, finish) {
-    start = parseInt(start);
-    finish = parseInt(finish);
-    $('#plist').find('tr').each(function(i, e) {
-      if(i < start || i > finish) {
-        table.row(i).remove();
-        console.log('ha!');
-      }
-    });
-    table.draw();
-};*/
-
+};
+ 
 /*
  * Load a patient view
  */
@@ -154,5 +190,5 @@ function resetScroll() {
   document.body.scrollTop = document.documentElement.scrollTop = 0;
 }
 
-module.exports = { 'loadView': loadView, 'saveUserInfo': saveUserInfo, 'loadIndex': loadIndex, 'results': results };
+module.exports = { 'loadView': loadView, 'saveUserInfo': saveUserInfo, 'loadIndex': loadIndex, 'loadList': loadList, 'results': results };
 
